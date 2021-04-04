ae83f74729c8be46c034807aed30173987851354
Steve Brownlee <chortlehoort@gmail.com>
2021-04-03 19:29:32 -0500

    Initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,18 @@
+
+
+const mainContainer = document.querySelector("#container")
+
+const applicationHTML = `
+<h1>Maude and Merle's Sink Repair</h1>
+<article class="serviceForm">
+
+</article>
+
+<article class="serviceRequests">
+    <h2>Service Requests</h2>
+
+</article>
+`
+
+mainContainer.innerHTML = applicationHTML
+
