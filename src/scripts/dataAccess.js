65e3b60ed5376e3ea0dca3becd3e50703470ac6a
Steve Brownlee <chortlehoort@gmail.com>
2021-04-07 09:00:05 -0500

    Main component

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -15,14 +15,20 @@ export const fetchRequests = () => {
         )
 }
 
-export const sendRequest = () => {
-    return fetch(`${API}/requests`, )
+export const sendRequest = (userServiceRequest) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userServiceRequest)
+    }
+
+    return fetch(`${API}/requests`, fetchOptions)
         .then(response => response.json())
-        .then(
-            (serviceRequests) => {
-                applicationState.requests = serviceRequests
-            }
-        )
+        .then(() => {
+
+        })
 }
 
 export const getRequests = () => {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,15 @@
 import { fetchRequests } from "./dataAccess.js"
-import { Requests } from "./Requests.js"
-import { ServiceForm } from "./ServiceForm.js"
+import { SinkRepair } from "./SinkRepair.js"
 
 
 const mainContainer = document.querySelector("#container")
 
-fetchRequests()
-    .then(
+const render = () => {
+    fetchRequests().then(
         () => {
-            const applicationHTML = `
-                <h1>Maude and Merle's Sink Repair</h1>
-                <section class="serviceForm">
-                    ${ServiceForm()}
-                </section>
-
-                <section class="serviceRequests">
-                    <h2>Service Requests</h2>
-                    ${Requests()}
-                </section>
-            `
-
-            mainContainer.innerHTML = applicationHTML
+            mainContainer.innerHTML = SinkRepair()
         }
     )
+}
 
+render()
