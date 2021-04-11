79efcd7e89fc824b49d7a651d19822d1e1172189
Steve Brownlee <chortlehoort@gmail.com>
2021-04-11 04:45:37 +0000

    Script for running via node

---
+++ api/repair-api.js
@@ -0,0 +1,24 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('database.json')
+
+//const middlewares = jsonServer.defaults({ noCors: true })
+const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 8089
+
+server.use(middlewares)
+
+server.use((req, res, next) => {
+  if (req.method === 'GET') {
+    next()
+  } else {
+    res.sendStatus(401)
+  }
+})
+
+
+server.use(router)
+
+server.listen(port, () => {
+    console.log('JSON Server is running')
+})
